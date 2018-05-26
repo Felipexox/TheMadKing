229991e0bbb84c8abc48f99ad988813db2bd223e
Gabriel Dias <gaabrielmooraes@gmail.com>
2018-05-26 11:30:26 -0300

    [Commit}     -- Added InputTeste Scene     -- Added Player script     -- Added InputController Script Class

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController {
+
+    
+    public Vector2 GetDirection()
+    {
+        Vector2 direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+            direction.x = -1;
+        if (Input.GetAxis("Vertical") != 0)
+            direction.y = Input.GetAxis("Vertical");
+
+        return direction;
+    }
+
+
+}
