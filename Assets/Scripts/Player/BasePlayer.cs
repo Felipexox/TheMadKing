32c88b051b1ef834ad2cf6759078a5c22ceb162c
Gabriel Dias <gaabrielmooraes@gmail.com>
2018-05-26 12:21:26 -0300

    [Commit]    -- Removed PlayerScript    -- Added Movement in BasePlayer

--- Assets/Scripts/Player/BasePlayer.cs
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class BasePlayer : MonoBehaviour {
 
     InputController _inputController;
 
@@ -13,24 +13,43 @@ public class Player : MonoBehaviour {
     [SerializeField]
     float runSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         _inputController = new InputController(KeyCode.A, KeyCode.W, KeyCode.D, KeyCode.S, KeyCode.Space, KeyCode.LeftShift, walkSpeed, runSpeed);
         rigidBody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Move(_inputController.GetDirection(), _inputController.GetSpeed());
 
-	}
+    }
 
 
     void Move(Vector3 direction, float velocity)
     {
         direction = direction.normalized * velocity;
         rigidBody.velocity = direction;
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        BaseInteraction baseInteraction = other.GetComponent<BaseInteraction>();
+        if(baseInteraction != null)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Interact(baseInteraction);
+            }
+        }
+    }
+    void Interact(BaseInteraction ObjInteract)
+    {
+         ObjInteract.Interact();
     }
 
 }
