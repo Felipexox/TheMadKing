7b4f9b90af127e1e4b6273a2f504929b65737732
Felipexox <felipeaa100@gmail.com>
2018-05-26 11:49:32 -0300

    [Commit]   - Changed Player.cs To BasePlayer.cs   - Implement interaction to player

---
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayer : MonoBehaviour {
+
+    InputController _inputController;
+
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        _inputController = new InputController();
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move(_inputController.GetDirection());
+
+	}
+
+
+    void Move(Vector2 direction)
+    {
+        //rigidBody.MovePosition((Vector2)transform.position + direction);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        BaseInteraction baseInteraction = other.GetComponent<BaseInteraction>();
+        if(baseInteraction != null)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Interact(baseInteraction);
+            }
+        }
+    }
+    void Interact(BaseInteraction ObjInteract)
+    {
+         ObjInteract.Interact();
+    }
+
+}
