58a5e67dfba08f4366b8c7d46b85735ac62cd3e9
Felipexox <felipeaa100@gmail.com>
2018-05-26 11:31:48 -0300

    [Commit]   - Added BaseInteraction to interactions   - Added Cooker class

---
+++ Assets/Scripts/Base/BaseInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInteraction : MonoBehaviour {
+
+    public virtual void Interact()
+    {
+
+    }
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entrou no " + this.name);
+    }
+}
---
+++ Assets/Scripts/InteractionsObject/Cooker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooker : BaseInteraction {
+
+    public override void Interact()
+    {
+        Debug.Log("Interacted");
+    }
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+}
